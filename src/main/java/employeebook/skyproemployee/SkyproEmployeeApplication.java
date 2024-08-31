43136d9cc2164012f47f437fff715b12581ed958
Unique-Kris <titina91@mail.ru>
2024-08-31 17:30:15 +0300

    HW2.6

---
+++ src/main/java/employeebook/skyproemployee/SkyproEmployeeApplication.java
@@ -0,0 +1,13 @@
+package employeebook.skyproemployee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproEmployeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/employeebook/skyproemployee/SkyproEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package employeebook.skyproemployee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproEmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
